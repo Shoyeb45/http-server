1d2f41537085998028e8fdf880cdf6ea6243ca4e
Shoyeb45 <shoyebff45@gmail.com>
2025-11-18 20:43:31 +0530

    Reading header

--- df.cpp
+++
@@ -1,9 +0,0 @@
-# include<iostream>
-using namespace std;
-
-int main() {
-    std::string url = "/echo/abc";
-    if (("/echo/")) {
-
-    }
-}
--- src/main.cpp
+++ src/main.cpp
@@ -7,83 +7,134 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netdb.h>
-# include<vector>
+#include <vector>
 
+std::vector<std::string> split_with_crlf(std::vector<char> &buf)
+{
+  std::vector<std::string> ans;
+  std::string temp = "";
+  for (int i = 0; i < buf.size(); i++)
+  {
+    if (i + 1 < buf.size())
+    {
+      std::string str_temp = std::to_string(buf[i]) + std::to_string(buf[i + 1]);
+      if (str_temp == "\r\n")
+      {
+        ans.push_back(temp);
+        temp = "";
+      }
+    }
+    temp += buf[i];
+  }
+  ans.push_back(temp);
+  return ans;
+}
 
-
-
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
   // Flush after every std::cout / std::cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
-  
+
   // You can use print statements as follows for debugging, they'll be visible when running tests.
   std::cout << "Logs from your program will appear here!\n";
 
   // Uncomment this block to pass the first stage
   //
   int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-  if (server_fd < 0) {
-  	std::cerr << "Failed to create server socket\n";
-  	return 1;
+  if (server_fd < 0)
+  {
+    std::cerr << "Failed to create server socket\n";
+    return 1;
   }
-  
+
   // Since the tester restarts your program quite often, setting SO_REUSEADDR
   // ensures that we don't run into 'Address already in use' errors
   int reuse = 1;
-  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
+  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0)
+  {
     std::cerr << "setsockopt failed\n";
     return 1;
   }
-  
+
   struct sockaddr_in server_addr;
   server_addr.sin_family = AF_INET;
   server_addr.sin_addr.s_addr = INADDR_ANY;
   server_addr.sin_port = htons(4221);
-  
-  if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) != 0) {
+
+  if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) != 0)
+  {
     std::cerr << "Failed to bind to port 4221\n";
     return 1;
   }
-  
+
   int connection_backlog = 5;
-  if (listen(server_fd, connection_backlog) != 0) {
+  if (listen(server_fd, connection_backlog) != 0)
+  {
     std::cerr << "listen failed\n";
     return 1;
   }
-  
+
   struct sockaddr_in client_addr;
   int client_addr_len = sizeof(client_addr);
-  
+
   std::cout << "Waiting for a client to connect...\n";
-  
-  int client_socket = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
+
+  int client_socket = accept(server_fd, (struct sockaddr *)&client_addr, (socklen_t *)&client_addr_len);
   std::cout << "Client connected\n";
   std::vector<char> buf(5000);
-  
+
   int bytes = recv(client_socket, buf.data(), buf.size(), 0);
-  
+
   std::string url = "";
-  for (int i = 4; i < 5000; i++) {
-    if (buf[i] == ' ') {
+  for (int i = 4; i < 5000; i++)
+  {
+    if (buf[i] == ' ')
+    {
       break;
     }
     url += buf[i];
   }
 
-  if (url.starts_with("/echo/")) {
+  if (url.starts_with("/echo/"))
+  {
     int idx = url.find_last_of('/');
     std::string temp = url.substr(idx + 1);
-    std::cout << temp << "\n";
     std::string response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ";
     response += std::to_string(temp.size());
     response += "\r\n\r\n" + temp;
+
     send(client_socket, response.c_str(), response.size() + 1, 0);
   }
   
-  if (url == "/") {
+  if (url.starts_with("/user-agent"))
+  {
+    std::string requests = buf.data();
+    
+    int idx_of_user = requests.find("User-Agent");
+    int idx = requests.find(":", idx_of_user);
+    std::string str_prm = "";
+
+    for (int i = idx + 2;; i++)
+    {
+      if (requests[i] == '\r' && requests[i + 1] == '\n')
+      {
+        break;
+      }
+      str_prm += requests[i];
+    }
+    std::string response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ";
+    response += std::to_string(str_prm.size());
+    response += "\r\n\r\n" + str_prm;
+    send(client_socket, response.c_str(), response.size() + 1, 0);
+  }
+
+  if (url == "/")
+  {
     send(client_socket, "HTTP/1.1 200 OK\r\n\r\n", 20, 0);
-  } else {
+  }
+  else
+  {
     send(client_socket, "HTTP/1.1 404 Not Found\r\n\r\n", 27, 0);
   }
 
